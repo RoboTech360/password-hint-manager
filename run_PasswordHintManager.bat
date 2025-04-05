ab081c21b394b145f7698156ae5b65495f4388d7
Malik <107283742+RoboTech360@users.noreply.github.com>
2025-04-05 17:55:57 -0400

    Create run_PasswordHintManager.bat

---
+++ run_PasswordHintManager.bat
@@ -0,0 +1,11 @@
+@echo off
+# option1: comment this if using option2
+python src\main_PasswordHintManager.py
+
+# option2:
+# Python is not added to the system's PATH variable then uncomment below line
+#"C:\path\to\python.exe" src\main_PasswordHintManager.py
+
+pause
+
+
